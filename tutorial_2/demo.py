a88af1c907332a633c0dc095124140a4531fa0fd
jingx <50790703+chooron@users.noreply.github.com>
2024-03-14 16:44:17 +0800

    上传第二次课程内容

---
+++ tutorial_2/demo.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+
+"""
+@author: jingxin
+@license: MIT
+@contact: jingxin0107@qq.com
+@file: demo.py
+@date: 2024/3/14 16:27
+@desc: 水库调节计算对象化封装练习
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+
+# 水库基本信息
+name = "黄金峡水库"
+idx = "10001"
+init_storage = 0.694
+
+# 水库特征曲线信息
+levels = [22, 24, 26, 28, 30, 32]
+storages = [0.61, 0.694, 0.876, 1.133, 1.450, 1.852]
+storage_itp = interp1d(levels, storages, fill_value='extrapolate')
+level_itp = interp1d(storages, levels, fill_value='extrapolate')
+
+# 入库流量和出库流量数据
+inflow = np.array([28, 69, 105, 367, 1320, 2440, 2760, 3020,
+                   3140, 2900, 2750, 1870, 1300, 1100, 980, 820])
+outflow = np.ones_like(inflow) * 1500
+
+storage_list = []
+level_list = []
+# 开始调节计算
+cur_storage = init_storage
+for i, o in zip(inflow, outflow):
+    tmp_change_storage = (i - o) * 3600 / 1e8
+    cur_storage = cur_storage + tmp_change_storage
+    cur_level = level_itp(cur_storage)
+    storage_list.append(cur_storage)
+    level_list.append(cur_level)
+
+# 绘制水库水位过程曲线
+plt.plot(level_list)
+plt.show()
